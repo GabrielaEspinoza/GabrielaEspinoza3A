839dbdb9ea07fe1f9034b79e2b36dd86af523bad
GabrielaEspinoza <56659435+GabrielaEspinoza@users.noreply.github.com>
2019-10-16 19:10:32 -0500

    Add files via upload

---
+++ Tarea6/Tarea6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//6. Modificar el ejercicio anterior para que aparezca un rombo. RESUELTO
+
+namespace Tarea6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el número de amplitud que tendra su rombo:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= numero; i++)
+            {
+                for (int b = 0; b < i; b++)
+                {
+                    Console.Write("*");
+                }
+                for (int a = numero; a < i; a++)
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = numero; i >= 0; i--)
+            {
+                for (int b = 0; b < i; b++)
+                {
+                    Console.Write("*");
+                }
+                for (int a = numero; a < i; a++)
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
