65ca4b3eac26ed7d33b3560fdb351414f2fa2a3e
GabrielaEspinoza <56659435+GabrielaEspinoza@users.noreply.github.com>
2019-10-16 19:09:58 -0500

    Add files via upload

---
+++ Tarea5/Tarea5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+//5. Crear el programa asteriscos4 en el que se introduce un número entero y se crea una pirámide de
+//asteriscos.Por ejemplo, si se introduce el 6, el resultado sería:
+// RESUELTO 
+namespace Tarea5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el número de amplitud que tendra su prirámide:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= numero; i++)
+            {
+                for (int b = 0; b < i; b++)
+                {
+                    Console.Write(" * ");
+                }
+                for (int a = numero; a < i; a++)
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
