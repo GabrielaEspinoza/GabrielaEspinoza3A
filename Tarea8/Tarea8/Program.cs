227fc6bdef95d13853cbe24499fc8f0ddf244962
GabrielaEspinoza <56659435+GabrielaEspinoza@users.noreply.github.com>
2019-10-16 19:25:35 -0500

    Add files via upload

---
+++ Tarea8/Tarea8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//8. Crear un programa en C# que lea un número entero y positivo y que escriba tres columnas. La primera
+//cuenta desde uno hasta el número escrito contando de uno en uno; la segunda columna cuenta de dos
+//en dos y la tercera de tres.
+namespace Tarea8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Digite la cantidad: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int num1 = 0, num2=0, num3=0, num4=0;
+            do
+            {
+                num2++;
+                num3 = num3 + 2;
+                num4 = num4 + 3;
+                Console.WriteLine(num2+"      " +num3+ "      " +num4+ "      ");
+                num1++;
+            } while (num1 < numero);
+            Console.ReadKey();
+            }
+        }
+    }
+
+
+
