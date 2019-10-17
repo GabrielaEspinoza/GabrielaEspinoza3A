d483a1504aa1c9a40cbce9789f8a11b50f3e1b52
GabrielaEspinoza <56659435+GabrielaEspinoza@users.noreply.github.com>
2019-10-16 19:27:21 -0500

    Add files via upload

---
+++ Tarea9/Tarea9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//9. Crear el programa tablaMultiplicar que escriba la tabla 
+//de multiplicar del número 1 al número 15.
+
+namespace Tarea9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            for (int i =1; i<= 15; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", numero, i, (numero * i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
